651ebc10665b401815fcc945cfa8d00ba0b2e100
Misha <opmisha@gmail.com>
2022-11-14 22:04:06 +0200

    added update-file-I10h

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1,3 +1,4 @@
+
 // GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -19,7 +20,7 @@ class S {
 
   static S get current {
     assert(_current != null,
-        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
     return _current!;
   }
 
@@ -42,12 +43,152 @@ class S {
   static S of(BuildContext context) {
     final instance = S.maybeOf(context);
     assert(instance != null,
-        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
     return instance!;
   }
 
   static S? maybeOf(BuildContext context) {
     return Localizations.of<S>(context, S);
+  }
+
+  /// `Help army`
+  String get helpArmy {
+    return Intl.message(
+      'Help army',
+      name: 'helpArmy',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `russia's losses`
+  String get ruLosses {
+    return Intl.message(
+      'russia\'s losses',
+      name: 'ruLosses',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Personnel`
+  String get personnel {
+    return Intl.message(
+      'Personnel',
+      name: 'personnel',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Tanks`
+  String get tanks {
+    return Intl.message(
+      'Tanks',
+      name: 'tanks',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Armored combat vehicles`
+  String get bbm {
+    return Intl.message(
+      'Armored combat vehicles',
+      name: 'bbm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Artillery systems`
+  String get artillery {
+    return Intl.message(
+      'Artillery systems',
+      name: 'artillery',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `MLRS`
+  String get mlrs {
+    return Intl.message(
+      'MLRS',
+      name: 'mlrs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Air defense systems`
+  String get ads {
+    return Intl.message(
+      'Air defense systems',
+      name: 'ads',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Aircrafts`
+  String get aircrafts {
+    return Intl.message(
+      'Aircrafts',
+      name: 'aircrafts',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Helicopters`
+  String get helicopters {
+    return Intl.message(
+      'Helicopters',
+      name: 'helicopters',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Tactical Unmanned Aircraft`
+  String get drone {
+    return Intl.message(
+      'Tactical Unmanned Aircraft',
+      name: 'drone',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ship and boasts`
+  String get ship {
+    return Intl.message(
+      'Ship and boasts',
+      name: 'ship',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Fuel tankers and vehicles`
+  String get tankers {
+    return Intl.message(
+      'Fuel tankers and vehicles',
+      name: 'tankers',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Quantity per day`
+  String get wuantityDay {
+    return Intl.message(
+      'Quantity per day',
+      name: 'wuantityDay',
+      desc: '',
+      args: [],
+    );
   }
 }
 
