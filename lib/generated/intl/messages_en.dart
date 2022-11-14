3a305b7e81a24101bfc44eca1afe7405ed36aa2b
Misha <opmisha@gmail.com>
2022-11-14 20:19:23 +0200

    added multi-language

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,42 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "ads": MessageLookupByLibrary.simpleMessage("Air defense systems"),
+        "aircrafts": MessageLookupByLibrary.simpleMessage("Aircrafts"),
+        "artillery": MessageLookupByLibrary.simpleMessage("Artillery systems"),
+        "bbm": MessageLookupByLibrary.simpleMessage("Armored combat vehicles"),
+        "drone":
+            MessageLookupByLibrary.simpleMessage("Tactical Unmanned Aircraft"),
+        "helicopters": MessageLookupByLibrary.simpleMessage("Helicopters"),
+        "helpArmy": MessageLookupByLibrary.simpleMessage("Help army"),
+        "mlrs": MessageLookupByLibrary.simpleMessage("MLRS"),
+        "personnel": MessageLookupByLibrary.simpleMessage("Personnel"),
+        "ruLosses": MessageLookupByLibrary.simpleMessage("russia\'s losses"),
+        "ship": MessageLookupByLibrary.simpleMessage("Ship and boasts"),
+        "tankers":
+            MessageLookupByLibrary.simpleMessage("Fuel tankers and vehicles"),
+        "tanks": MessageLookupByLibrary.simpleMessage("Tanks"),
+        "wuantityDay": MessageLookupByLibrary.simpleMessage("Quantity per day")
+      };
+}
---
+++ lib/generated/l10n.dart
@@ -0,0 +1,220 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Help army`
+  String get helpArmy {
+    return Intl.message(
+      'Help army',
+      name: 'helpArmy',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `russia's losses`
+  String get ruLosses {
+    return Intl.message(
+      'russia\'s losses',
+      name: 'ruLosses',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Personnel`
+  String get personnel {
+    return Intl.message(
+      'Personnel',
+      name: 'personnel',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Tanks`
+  String get tanks {
+    return Intl.message(
+      'Tanks',
+      name: 'tanks',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Armored combat vehicles`
+  String get bbm {
+    return Intl.message(
+      'Armored combat vehicles',
+      name: 'bbm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Artillery systems`
+  String get artillery {
+    return Intl.message(
+      'Artillery systems',
+      name: 'artillery',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `MLRS`
+  String get mlrs {
+    return Intl.message(
+      'MLRS',
+      name: 'mlrs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Air defense systems`
+  String get ads {
+    return Intl.message(
+      'Air defense systems',
+      name: 'ads',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Aircrafts`
+  String get aircrafts {
+    return Intl.message(
+      'Aircrafts',
+      name: 'aircrafts',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Helicopters`
+  String get helicopters {
+    return Intl.message(
+      'Helicopters',
+      name: 'helicopters',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Tactical Unmanned Aircraft`
+  String get drone {
+    return Intl.message(
+      'Tactical Unmanned Aircraft',
+      name: 'drone',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ship and boasts`
+  String get ship {
+    return Intl.message(
+      'Ship and boasts',
+      name: 'ship',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Fuel tankers and vehicles`
+  String get tankers {
+    return Intl.message(
+      'Fuel tankers and vehicles',
+      name: 'tankers',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Quantity per day`
+  String get wuantityDay {
+    return Intl.message(
+      'Quantity per day',
+      name: 'wuantityDay',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'ru'),
+      Locale.fromSubtags(languageCode: 'uk'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
