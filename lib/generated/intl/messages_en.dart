447188471cfc4e5d9101653934a8785d89c90b2c
Misha <opmisha@gmail.com>
2022-11-18 19:39:21 +0200

    fix multilingual bugs

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -21,5 +21,22 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "ads": MessageLookupByLibrary.simpleMessage("Air defense systems"),
+        "aircrafts": MessageLookupByLibrary.simpleMessage("Aircrafts"),
+        "artillery": MessageLookupByLibrary.simpleMessage("Artillery systems"),
+        "bbm": MessageLookupByLibrary.simpleMessage("Armored combat vehicles"),
+        "drone":
+            MessageLookupByLibrary.simpleMessage("Tactical Unmanned Aircraft"),
+        "helicopters": MessageLookupByLibrary.simpleMessage("Helicopters"),
+        "helpArmy": MessageLookupByLibrary.simpleMessage("Help army"),
+        "mlrs": MessageLookupByLibrary.simpleMessage("MLRS"),
+        "personnel": MessageLookupByLibrary.simpleMessage("Personnel"),
+        "ruLosses": MessageLookupByLibrary.simpleMessage("russia\'s losses"),
+        "ship": MessageLookupByLibrary.simpleMessage("Ship and boasts"),
+        "tankers":
+            MessageLookupByLibrary.simpleMessage("Fuel tankers and vehicles"),
+        "tanks": MessageLookupByLibrary.simpleMessage("Tanks"),
+        "wuantityDay": MessageLookupByLibrary.simpleMessage("Quantity per day")
+      };
 }
